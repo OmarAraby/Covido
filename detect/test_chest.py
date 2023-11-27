e1297144c7e7c91fa830a74eb5d285783c245135
OmarAraby <omararaby789@gmail.com>
2023-11-27 02:46:16 +0200

    Handel X-ray Model

--- detect/test_chest.py
+++ detect/test_chest.py
@@ -1,23 +1,44 @@
-import xray_classification
+from keras.models import load_model
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+from keras.preprocessing.image import img_to_array, load_img
 
 
 
-
-loaded_model = load_model("xray_model.h5")
-loaded_model.summary()
-loaded_model.evaluate(test_ds, return_dict=True)
-#loss, accuracy = loaded_model.evaluate(test_ds, return_dict=True))
+loaded_model = load_model("D:/Tutorials/covido/src/detect/xray_model.h5")
 
 
 
-###### Test
-for image, label in test_ds.take(1):
-    plt.imshow(image[0] / 255.0)
-    plt.title(CLASS_NAMES[label[0].numpy()])
+image_path = "/content/Viral Pneumonia-10.png"
+img = load_img(image_path, target_size=(180, 180))
 
-prediction = model.predict(test_ds.take(1))[0]
+# Display the image
+plt.imshow(img)
+plt.axis('off')
+plt.show()
+
+# Load the pre-trained model
+model_path = "/content/drive/MyDrive/xray/xray_model.h5"
+#model_path = "/content/xray_modelxx.h5"
+loaded_model = load_model(model_path)
+
+# Preprocess the image
+img_array = img_to_array(img)
+img_array = np.expand_dims(img_array, axis=0)
+preprocessed_image = img_array / 255.0  # Normalize pixel values to the range of 0-1
+
+# Make a prediction
+prediction = loaded_model.predict(preprocessed_image)
+
+# Process the prediction
+predicted_class_index = np.argmax(prediction)
+class_labels = ['Normal', 'Pneumonia']  # Replace with your actual class labels
+predicted_label = class_labels[predicted_class_index]
+
+#print("Predicted class:", predicted_label)
+
 scores = [1 - prediction, prediction]
 
-for score, name in zip(scores, CLASS_NAMES):
+for score, name in zip(scores, class_labels):
     print("This image is %.2f percent %s" % ((100 * score), name))
--- detect/views.py
+++ detect/views.py
@@ -4,35 +4,28 @@ from .models import UploadedImage
 from django.urls import reverse
 from django.core.files.storage import FileSystemStorage  
 from django.conf import settings
-import os
+from tensorflow.python import keras
+from keras.preprocessing.image import img_to_array,load_img
+import numpy as np
+from keras.models import load_model
+import io
+
+# from PIL import Image
+
+# def load_img(image_path, target_size):
+#     return Image.open(image_path).resize(target_size)
 
 
 
 
+# from keras.layers import BatchNormalization ,Rescaling
+# custom_objects = {'BatchNormalization': BatchNormalization, 'Rescaling': Rescaling}
 
 
 
-# def detect(request):
 
-#     feedbacks = Feedback.objects.all()  
-#     images = ImageSlide.objects.all()  
-#     if request.method == 'POST':
-#         uploaded_image = request.FILES['image']
-       
+model  = load_model('D:/Tutorials/covido/src/detect/xray_modelxx.h5')
 
-#         # Save the uploaded image to a temporary location
-#         fs = FileSystemStorage()
-#         filename = fs.save(uploaded_image.name, uploaded_image)
-#         #Dvalue=XrayPreduction.XrayP(str(filename))
-
-#         # Construct the URL to the uploaded image
-#         result_url = fs.url(filename)
-
-#         # Redirect to 'detect:result_chest' with the image URL as a parameter
-#         return  render(request, 'result_chest.html', {'result_url': result_url,'feedbacks': feedbacks, 'images': images})
-
-#     # If the request method is not POST, render the form
-#     return render(request, 'detect.html')
 
 
 def detect(request):
@@ -42,23 +35,68 @@ def detect(request):
     if request.method == 'POST':
         uploaded_image = request.FILES['image']
 
-        # Save the uploaded image to a temporary location
-        # fs = FileSystemStorage()
-        # filename = fs.save(uploaded_image.name, uploaded_image)
-
-        # # Construct the URL to the uploaded image
-        # result_url = fs.url(filename)
-
+        
         # Create an instance of UploadedImage and save it to the database
         uploaded_image_instance = UploadedImage.objects.create(image=uploaded_image)
+        print('Image NAme :',uploaded_image_instance.image.name)
+      
 
-        context = {'uploaded_image': uploaded_image_instance,'feedbacks': feedbacks, 'images': images}
+        img = load_img(uploaded_image_instance.image.path, target_size=(180, 180))
+        
+        # # Preprocess the image
+        img_array = img_to_array(img)
+        img_array = np.expand_dims(img_array, axis=0)
+        preprocessed_image = img_array / 255.0  # Normalize pixel values to the range of 0-1
+
+
+        # # Make a prediction
+        prediction = model.predict(preprocessed_image)
+
+        # # Process the prediction
+        predicted_class_index = np.argmax(prediction)
+        class_labels = ['Normal', 'Pneumonia']  # Replace with your actual class labels
+        predicted_label = class_labels[predicted_class_index]
+
+
+        scores = [100 * (1 - prediction[0][0]), 100 * prediction[0][0]]
+
+        combined_data = list(zip(scores, class_labels))
+        for score, name in combined_data:
+            print("This image is %.2f percent %s" % ((score), name))
+
+
+        context = {
+            'uploaded_image': uploaded_image_instance,
+            'feedbacks': feedbacks,
+            'images': images,
+            'combined_data':combined_data,
+        }
 
         # Redirect to 'detect:result_chest' with the image URL as a parameter
         return  render(request, 'result_chest.html', context)
 
     # If the request method is not POST, render the form
     return render(request, 'detect.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -84,36 +122,4 @@ def detect(request):
 
 
 
-
-
-
-
-#import tensorflow as tf
-#from tensorflow.python import keras
-# import numpy as np
-# from keras_preprocessing.image import ImageDataGenerator
-
-
-# # Create your views here.
-
-
-# # Load the model
-# # Load your model
-#model = keras.models.load_model('xray_model.h5')
-
-# target_size = (224, 224)
-
-# def process_uploaded_image(uploaded_image):
-#    # Convert the uploaded image to a format compatible with model prediction
-#     img = ImageDataGenerator.load_img(uploaded_image, target_size=target_size)
-#     img_array = ImageDataGenerator.img_to_array(img)
-#     img_array = np.expand_dims(img_array, axis=0)
-#     img_array /= 255.0  # Normalize pixel values
-
-#     # Make predictions
-#     result = model.predict(img_array)
-
-#     # You may need to post-process the result based on your specific model and task
-
-#     return result
 
